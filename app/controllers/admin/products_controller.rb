422dc21eed6d9b8d516c23bb90d928299b548737
Knight~ <x665adex563@gmail.com>
2026-05-07 16:40:07 +0800

    feat(admin-products): implement  product creation flow (new/create)

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,2 +1,20 @@
 class Admin::ProductsController < ApplicationController
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to admin_products_path, notice: t("admin.products.flash.created")
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:title, :description, :quantity, :price)
+  end
 end
