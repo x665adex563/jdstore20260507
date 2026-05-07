7a374aa15c5ecc896f9d209f13b7eff0edc17b91
Knight~ <x665adex563@gmail.com>
2026-05-07 16:57:35 +0800

    feat(admin-products): implement  product editing(edit/update)

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,6 @@
 class Admin::ProductsController < ApplicationController
+  before_action :set_product, only: [:edit, :update]
+
   def new
     @product = Product.new
   end
@@ -16,9 +18,24 @@ class Admin::ProductsController < ApplicationController
     @products = Product.all
   end
 
+  def edit
+  end
+
+  def update
+    if @product.update(product_params)
+      redirect_to admin_products_path, notice: t("admin.products.flash.updated")
+    else
+      render :edit
+    end
+  end
+
   private
 
   def product_params
     params.require(:product).permit(:title, :description, :quantity, :price)
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
   end
 end
