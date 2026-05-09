6453b951db3f7e451ba9311f946c9149dc765f00
Knight~ <x665adex563@gmail.com>
2026-05-09 16:48:23 +0800

    feat(order): add payment actions to order page

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,12 @@ Rails.application.routes.draw do
       end
     end
     resources :cart_items, only: [:create, :update, :destroy]
-    resources :orders
+    resources :orders, param: :token do
+      member do
+        post :pay_with_creditcard
+        post :pay_with_ewallet
+      end
+    end
     namespace :account do
       resources :orders
     end
