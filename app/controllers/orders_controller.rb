ca026c7eefb2fb19f577d63134bdbe27e0471eca
Knight~ <x665adex563@gmail.com>
2026-05-09 15:57:44 +0800

    feat(order): display order with ProductList details in show action

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -24,6 +24,11 @@ class OrdersController < ApplicationController
     end
   end
 
+  def show
+    @order = Order.find(params[:id])
+    @product_lists = @order.product_lists
+  end
+
   private
 
   def order_params
