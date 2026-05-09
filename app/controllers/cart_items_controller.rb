5fd8eede6d3dec2a8937f57a013a80c144968688
Knight~ <x665adex563@gmail.com>
2026-05-09 12:58:15 +0800

    feat(cart): block add to cart when product is out of stock

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,10 +1,12 @@
 class CartItemsController < ApplicationController
   def create
     @product = Product.find(params[:product_id])
-    current_cart.add_product(@product)
 
-    redirect_back fallback_location: root_path,
-                  notice: t("cart_items.flash.added")
+    if current_cart.add_product(@product)
+      redirect_back fallback_location: root_path, notice: t("cart_items.flash.added")
+    else
+      redirect_back fallback_location: root_path, alert: t("cart_items.flash.out_of_stock")
+    end
   end
 
   def destroy
