55e084bbb7a9b0faf727b3ed7cbe04a96da864be
Knight~ <x665adex563@gmail.com>
2026-05-09 16:22:04 +0800

    fix(account-order): correct created_at typo

--- app/controllers/account/orders_controller.rb
+++ app/controllers/account/orders_controller.rb
@@ -1,5 +1,5 @@
 class Account::OrdersController < ApplicationController
   def index
-    @orders = current_user.orders.order(create_at: :desc)
+    @orders = current_user.orders.order(created_at: :desc)
   end
 end
