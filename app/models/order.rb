e7f72a582f48e3499fd5cbaf1af2b08306dc06a2
Knight~ <x665adex563@gmail.com>
2026-05-09 13:27:59 +0800

    feat(order): add presence validations and user association

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,6 @@
 class Order < ApplicationRecord
+  validates :billing_name, :billing_address, presence: true
+  validates :shipping_name, :shipping_address, presence: true
+
+  belongs_to :user
 end
