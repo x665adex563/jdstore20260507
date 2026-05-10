ece551aa7ff4eaee7d51f869c6dfcfe76952771e
Knight~ <x665adex563@gmail.com>
2026-05-10 12:11:15 +0800

    feat(aasm): install aasm and add orders.aasm_state column with index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_05_09_083237) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_10_040952) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
@@ -55,6 +55,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_05_09_083237) do
   end
 
   create_table "orders", force: :cascade do |t|
+    t.string "aasm_state", default: "order_placed"
     t.string "billing_address"
     t.string "billing_name"
     t.datetime "created_at", null: false
@@ -66,6 +67,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_05_09_083237) do
     t.integer "total", default: 0
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.index ["aasm_state"], name: "index_orders_on_aasm_state"
     t.index ["token"], name: "index_orders_on_token", unique: true
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
