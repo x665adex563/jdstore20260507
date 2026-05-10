3f1d46827de9db6c8c4c01feef51ff0c4e2e579a
Knight~ <x665adex563@gmail.com>
2026-05-10 11:53:14 +0800

    fix(mail): clean up OrderMailerformatting and syntax  issues

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,9 +1,9 @@
 class OrderMailer < ApplicationMailer
   def notify_order_placed(order)
-    @order       = order
-    @user        = order.user
+    @order = order
+    @user = order.user
     @product_lists = @order.product_lists
-﻿
-    mail(to: @user.email , subject: t("order_mailer.notify_order_placed.subject"))
+
+    mail(to: @user.email, subject: t("order_mailer.notify_order_placed.subject"))
   end
 end
