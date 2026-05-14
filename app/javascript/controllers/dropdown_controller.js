99a96a15884c981d98d1269808d5165c60091c66
Knight~ <x665adex563@gmail.com>
2026-05-14 12:14:30 +0800

    fix(locale-switcher): make dropdown work on mobile using click toggle

---
+++ app/javascript/controllers/dropdown_controller.js
@@ -0,0 +1,35 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["menu"]
+
+  connect() {
+    this.opened = false
+    document.addEventListener("click", this.handleOutsideClick)
+  }
+
+  disconnect() {
+    document.removeEventListener("click", this.handleOutsideClick)
+  }
+
+  toggle(event) {
+    event.stopPropagation()
+    this.opened ? this.close() : this.open()
+  }
+
+  open() {
+    this.menuTarget.classList.remove("hidden")
+    this.opened = true
+  }
+
+  close() {
+    this.menuTarget.classList.add("hidden")
+    this.opened = false
+  }
+
+  handleOutsideClick = (event) => {
+    if (!this.element.contains(event.target)) {
+      this.close()
+    }
+  }
+}
